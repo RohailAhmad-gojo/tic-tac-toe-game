321e6e826646acdb038472d2f96cd9b4b26f7e34
RohailAhmad-gojo <rohailgojali@gmail.com>
2022-10-13 13:57:58 +0500

    uploading

---
+++ src/components/Square.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import "../App.css";
+
+function Square({val, chooseSquare}) {
+  return (
+    <div           className="square"
+
+    onClick={chooseSquare}>{val} 
+     </div>
+  )
+}
+
+export default Square;
